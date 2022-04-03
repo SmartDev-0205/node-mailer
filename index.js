45747605fec711c64ba8abbef9db15d22993a9e0
unknown <smartdevpro001@gmail.com>
2022-04-03 10:31:56 -0700

    node mailer

---
+++ index.js
@@ -0,0 +1,55 @@
+const https = require('https');
+var express = require('express'),
+    app = express(),
+    port = process.env.PORT || 5000;
+
+app.use(function (re, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "*")
+    next()
+})
+app.use(express.json());
+app.post('/sendmail', (req, res) => {
+    // console.log("start ==================================",req.body);
+
+    // var postData = JSON.stringify({
+    //     "email": "smartdevpro001@gmail.com",
+    //     "html": "hello howaaaaaaaaaaaaaa are you",
+    //     "title": "Get Wallet Information",
+    // });
+    var postData =JSON.stringify( req.body);
+    var options = {
+        hostname: 'appapi.anexacargo.com',
+        port: 443,
+        path: '/sendmails',
+        method: 'POST',
+        headers: {
+            Authorization:
+                "Basic QWV4cHJlc3M6ZTcwNmRkOTEtZmFlYS00ZWJiLWI5N2EtMDYwMjQxMDg1ZWVm",
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "*"
+        }
+    };
+    
+    var req = https.request(options, (res) => {
+        console.log('statusCode:', res.statusCode);
+        console.log('headers:', res.headers);
+    
+        res.on('data', (d) => {
+            process.stdout.write(d);
+        });
+    });
+    
+    req.on('error', (e) => {
+        console.error(e);
+    });
+    
+    req.write(postData);
+    req.end();
+});
+
+app.listen(port);
+
+console.log('todo list RESTful API server started on: ' + port);	
